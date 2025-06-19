a5076c58793fc6f30cef7fb4ec8b108e4b8388b6
tanuj1221 <75058861+tanuj1221@users.noreply.github.com>
2025-06-19 12:42:55 +0530

    tanuj

--- apps/tif_to_picker/urls.py
+++ apps/tif_to_picker/urls.py
@@ -1,13 +1,13 @@
 # apps\tif_to_picker\urls.py
 from django.urls import path
-from .views import upload_tiff,single_layer_color_picker,export_tiff,process_svg_upload,analyze_color
+from .views import upload_tiff,single_layer_color_picker,export_file,process_svg_upload,analyze_color
 from . import views
 from apps.subscription_module.views import create_favorite_palette,remove_favorite_palette,get_favorites
 urlpatterns = [
     path('', upload_tiff, name='upload_tiff'),
     path('projects/<int:user_id>/<int:project_id>/edit/', upload_tiff, name='edit-project'),
     path('color-picker/', single_layer_color_picker, name='single_layer_color_picker'),
-    path('export_tiff/', export_tiff, name='export_tiff'),
+    path('export_file/', export_file, name='export_tiff'),
     path('upload-svg/', process_svg_upload, name='process_svg_upload'),
     path('analyze-color/', analyze_color, name='analyze-color'),
     path('inspiration-pdfs/', views.inspiration_view, name='inspiration_pdfs'),
